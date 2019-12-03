5e2e22cd25706e07ef9440f70d9f3bcbea07bc8b
James Bek <48003562+JJRIKI@users.noreply.github.com>
2019-12-03 14:42:45 -0800

    Update index.php

--- HoVeR_php/report_violations/index.php
+++ HoVeR_php/report_violations/index.php
@@ -62,7 +62,7 @@
                         $input_violator = $_POST['c_users'];
                         $dbconn = pg_connect("host=bminer-apps port=5444 dbname=compliance user=compliance password=cs1230");
     
-                        $search_name = 'SELECT user_id FROM c_users WHERE first_name = AND last_name = $1';
+                        $search_name = 'SELECT user_id FROM c_users WHERE first_name = $1 AND last_name = $2';
                         $search_unit = 'SELECT user_id FROM Unit WHERE unit_num = $1';
                         $search_licence = 'SELECT user_id FROM Car WHERE licence_plate = $1';
     
