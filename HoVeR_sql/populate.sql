957fe0a160ad574efcae9c547d46477f1ccfcd09
James Bek <48003562+JJRIKI@users.noreply.github.com>
2019-12-03 12:38:33 -0800

    Update populate.sql

--- HoVeR_sql/populate.sql
+++ HoVeR_sql/populate.sql
@@ -37,16 +37,21 @@ VALUES
 
 INSERT INTO Violation_category (violation_category_id, violation_name, num_for_fine, num_for_call, num_for_email, fine_baseline) 
 VALUES
-	(1, 'Pet', 3, 2, 1, 100);
+	(1, 'Parking', 3, 2, 1, 100);
 
 INSERT INTO Violation_category (violation_category_id, violation_name, num_for_fine, num_for_call, num_for_email, fine_baseline)
 VALUES
-        (2, 'Parking', 3, 2, 1, 300);
+        (2, 'Noise', 3, 2, 1, 200);
 
 INSERT INTO Violation_category (violation_category_id, violation_name, num_for_fine, num_for_call, num_for_email, fine_baseline)
 VALUES
-        (3, 'Noise', 3, 2, 1, 200);
+        (3, 'Common Area Usage', 3, 2, 1, 300);
 
+INSERT INTO Violation_category (violation_category_id, violation_name, num_for_fine, num_for_call, num_for_email, fine_baseline)
+        (4, 'Unit Conditions', 3, 2, 1, 400);
+
+INSERT INTO Violation_category (violation_category_id, violation_name, num_for_fine, num_for_call, num_for_email, fine_baseline)
+        (5, 'Noise', 3, 2, 1, 500);
 
 INSERT INTO Violations (user_id, violation_category_id, violation_date, description, reporter) 
 VALUES
