5372caff01860ed8f6360b4ba869529ed539ab7e
amydahlberg <49294266+amydahlberg@users.noreply.github.com>
2019-12-05 13:08:32 -0800

    deletiing files

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-$dbconn = pg_connect("host=bminer-apps port=5444 dbname=compliance user=compliance password=cs1230");
-//connect to a database named "mary" on the host "sheep" with a username and password
-
-?>
